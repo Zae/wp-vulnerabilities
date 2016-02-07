4df6de75a0bc444ee9028d6c292e0142278aa8fa
Ezra Pool <ezra@tsdme.nl>
2016-02-07 23:34:39 +0100

    Use the vulnDB service instead of a private method to filter vulnerabilities

--- src/Providers/Plugins/VulnDB.php
+++ src/Providers/Plugins/VulnDB.php
@@ -12,6 +12,7 @@ use GuzzleHttp\Pool;
 use Illuminate\Support\Collection;
 use Zae\WPVulnerabilities\Config;
 use Zae\WPVulnerabilities\Entities\Entity;
+use Zae\WPVulnerabilities\Services\VulnDBService;
 
 /**
  * Class VulnDB
@@ -24,26 +25,36 @@ class VulnDB
 	 * @var Client
 	 */
 	private $http;
+	
 	/**
 	 * @var Comparator
 	 */
 	private $semver;
+
 	/**
 	 * @var Config
 	 */
 	private $config;
 
 	/**
+	 * @var VulnDBService
+	 */
+	private $vulnDB;
+
+	/**
 	 * VulnDB constructor.
 	 *
-	 * @param Client $http
-	 * @param Comparator $semver
+	 * @param Client        $http
+	 * @param Comparator    $semver
+	 * @param Config        $config
+	 * @param VulnDBService $vulnDB
 	 */
-	public function __construct(Client $http, Comparator $semver, Config $config)
+	public function __construct(Client $http, Comparator $semver, Config $config, VulnDBService $vulnDB)
 	{
 		$this->http = $http;
 		$this->semver = $semver;
 		$this->config = $config;
+		$this->vulnDB = $vulnDB;
 	}
 
 	/**
@@ -84,7 +95,7 @@ class VulnDB
 				if ($response->getStatusCode() === 200) {
 					$response_object = json_decode((string)$response->getBody());
 
-					if ($this->isVulnerable($response_object->{$plugins[$index]->getName()}->vulnerabilities, $plugins[$index]->getVersion(), $vulnerabilities)) {
+					if ($this->vulnDB->isVulnerable($response_object->{$plugins[$index]->getName()}->vulnerabilities, $plugins[$index]->getVersion(), $vulnerabilities)) {
 						$plugins[$index]->vulnerable(Collection::make($vulnerabilities)->implode('title', ','));
 					}
 				}
@@ -92,21 +103,5 @@ class VulnDB
 		]))->promise()->wait();
 
 		return $plugins;
-	}
-
-	/**
-	 * @param      $vulnerabilities
-	 * @param      $version
-	 * @param null $found_vulnerabilities
-	 *
-	 * @return bool
-	 */
-	private function isVulnerable($vulnerabilities, $version, &$found_vulnerabilities = null)
-	{
-		$found_vulnerabilities = Collection::make($vulnerabilities)->filter(function($v) use ($version) {
-			return $this->semver->lessThan($version, $v->fixed_in);
-		});
-
-		return $found_vulnerabilities->count() > 0;
 	}
 }
